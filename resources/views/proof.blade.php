a197859ef4eae20ad7ea18a79171bc63ccae1cc7
DanielEspitia1507 <134316312+DanielEspitia1507@users.noreply.github.com>
2023-07-07 16:42:39 -0500

    Commit de FontAwesome

--- resources/views/proof.blade.php
+++ resources/views/proof.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://kit.fontawesome.com/42c51a431b.js" crossorigin="anonymous"></script>
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('fontawesome-free-6.4.0-web/css/all.min.css')}}">
     <title>Proof</title>
 </head>
 <body class="bg-white dark:bg-slate-900">
