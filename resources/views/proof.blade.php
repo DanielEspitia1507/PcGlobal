07ea66d4c07bde63ab6bd02a64b637c8abd8dfcd
DanielEspitia1507 <134316312+DanielEspitia1507@users.noreply.github.com>
2023-07-10 10:29:41 -0500

    Cambios CSS

--- resources/views/proof.blade.php
+++ resources/views/proof.blade.php
@@ -7,42 +7,45 @@
     <link rel="icon" href="{{asset('img/hamburger.ico')}}">
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{asset('fontawesome-free-6.4.0-web/css/all.min.css')}}">
-    <title>Proof</title>
+    <title>Restaurant</title>
 </head>
 <body class="flex flex-col min-h-screen bg-white dark:bg-slate-900">
     <header class="h-16 shadow-lg dark:shadow-cyan-500/25">
-        <nav class="mx-auto sm:mx-12 p-2 list-none flex gap-5 items-center h-full dark:text-white font-medium bg-transparent justify-around sm:justify-between">
+        <nav class="mx-auto sm:mx-12 2xl:mx-64 p-2 list-none flex gap-5 items-center h-full dark:text-white font-medium bg-transparent justify-around sm:justify-between text-md 2xl:text-xl">
             <li><a class="hover:text-gray-700 dark:hover:text-gray-500" href="{{route('index')}}"><i class="fa-solid fa-house"></i></a></li>
             <li><a class="hover:text-gray-700 dark:hover:text-gray-500" href="#"><i class="fa fa-regular fa-heart"></i></a></li>    
         </nav>
     </header>
     <main class="container my-10 text-justify dark:text-gray-200 mx-auto flex-grow">
-        <p class="mx-10">¿Listo para una experiencia culinaria llena de sabor y rapidez? En nuestro paraíso de comidas rápidas, te deleitarás con hamburguesas jugosas, crujientes papas fritas doradas y una amplia selección de tentadoras opciones. Nuestros ingredientes frescos y de calidad se combinan para crear obras maestras gastronómicas que deleitarán tu paladar. Desde clásicos atemporales hasta creaciones innovadoras, nuestra variedad de opciones te garantiza una experiencia única en cada visita. Además, nuestro amable personal te atenderá con una sonrisa, brindándote un servicio rápido y eficiente. Ven y descubre por qué somos el destino favorito de los amantes de la comida rápida. ¡No te arrepentirás!"
-        Recuerda que puedes ajustar el texto según tus necesidades y adaptarlo a tu estilo y oferta gastronómica. Si tienes más preguntas, estaré encantado de ayudarte. ¡Buen provecho!!</p>
-        <div class="grid grid-cols-1 sm:grid-cols-3 gap-x-5 mx-10">
-            <div>            
-                <a href="https://www.tripadvisor.co/Restaurants-g294074-zfd10907-Bogota-Hamburger.html" target="_blank"><img class="my-5 blur-none sm:blur-none sm:hover:blur-sm sm:hover:-rotate-3 transition-all duration-500 rounded-md hover:drop-shadow-md dark:hover:shadow-none cursor-pointer" width="450px" src="{{asset('img/hamburguesa.jpg')}}" alt=""></a>
-                <p class="text-center font-medium">Hamburguesa</p>
+        <p class="mx-10 text-md 2xl:text-2xl">¿Listo para una experiencia culinaria llena de sabor y rapidez? En nuestro paraíso de comidas rápidas, te deleitarás con hamburguesas jugosas, crujientes papas fritas doradas y una amplia selección de tentadoras opciones. Nuestros ingredientes frescos y de calidad se combinan para crear obras maestras gastronómicas que deleitarán tu paladar. Desde clásicos atemporales hasta creaciones innovadoras, nuestra variedad de opciones te garantiza una experiencia única en cada visita. Además, nuestro amable personal te atenderá con una sonrisa, brindándote un servicio rápido y eficiente. Ven y descubre por qué somos el destino favorito de los amantes de la comida rápida. ¡¡No te arrepentirás!!</p>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-5 mx-10">
+            <div>                
+                <img class="my-5 blur-none sm:blur-none sm:hover:blur-sm sm:hover:-rotate-3 transition-all duration-500 rounded-md hover:drop-shadow-md dark:hover:shadow-none cursor-pointer" width="450px" src="{{asset('img/hamburguesa.jpg')}}" alt="">
+                <p class="text-center font-medium 2xl:text-lg">Hamburguesa</p>
             </div>
             <div>            
                 <img class="my-5 blur-none sm:blur-none sm:hover:blur-sm sm:hover:-rotate-3 transition-all duration-500 rounded-md hover:drop-shadow-md dark:hover:shadow-none cursor-pointer" width="450px" src="{{asset('img/hot-dog.jpg')}}" alt="">
-                <p class="text-center font-medium">Hot-Dog</p>
+                <p class="text-center font-medium 2xl:text-lg">Hot-Dog</p>
             </div>
             <div>            
                 <img class="my-5 blur-none sm:blur-none sm:hover:blur-sm sm:hover:-rotate-3 transition-all duration-500 rounded-md hover:drop-shadow-md dark:hover:shadow-none cursor-pointer" width="450px" src="{{asset('img/pizza.png')}}" alt="">
-                <p class="text-center font-medium">Pizza</p>
+                <p class="text-center font-medium 2xl:text-lg">Pizza</p>
             </div>
             <div>            
                 <img class="my-5 blur-none sm:blur-none sm:hover:blur-sm sm:hover:-rotate-3 transition-all duration-500 rounded-md hover:drop-shadow-md dark:hover:shadow-none cursor-pointer" width="450px" src="{{asset('img/batidoChocolate.jpg')}}" alt="">
-                <p class="text-center font-medium">Batido de Chocolate</p>
+                <p class="text-center font-medium 2xl:text-lg">Batido de Chocolate</p>
             </div>
             <div>            
                 <img class="my-5 blur-none sm:blur-none sm:hover:blur-sm sm:hover:-rotate-3 transition-all duration-500 rounded-md hover:drop-shadow-md dark:hover:shadow-none cursor-pointer" width="450px" src="{{asset('img/rollitos.jpg')}}" alt="">
-                <p class="text-center font-medium">Rollitos de Jamón y Queso</p>
+                <p class="text-center font-medium 2xl:text-lg">Rollitos de Jamón y Queso</p>
             </div>
             <div>            
                 <img class="my-5 blur-none sm:blur-none sm:hover:blur-sm sm:hover:-rotate-3 transition-all duration-500 rounded-md hover:drop-shadow-md dark:hover:shadow-none cursor-pointer" width="450px" src="{{asset('img/sandwichQuesoParrilla.jpg')}}" alt="">
-                <p class="text-center font-medium">Sándwich de queso a la parrilla</p>
+                <p class="text-center font-medium 2xl:text-lg">Sándwich de queso a la parrilla</p>
+            </div>
+            <div>            
+                <img class="my-5 blur-none sm:blur-none sm:hover:blur-sm sm:hover:-rotate-3 transition-all duration-500 rounded-md hover:drop-shadow-md dark:hover:shadow-none cursor-pointer" width="450px" src="{{asset('img/palitosQueso.jpg')}}" alt="">
+                <p class="text-center font-medium 2xl:text-lg">Pallitos de Queso</p>
             </div>
         </div>
     </main>
