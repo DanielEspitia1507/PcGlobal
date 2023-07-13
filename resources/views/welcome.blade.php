9d79c7e9a5b5ba310fe8b2dcdc6641af91374179
DanielEspitia1507 <134316312+DanielEspitia1507@users.noreply.github.com>
2023-07-13 08:01:21 -0500

    Checkpoint :p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,13 @@
 <body class="flex flex-col min-h-screen">
     @include('navbar')
     <main class="container my-10 text-justify mx-auto flex-grow">
+        <div class="grid grid-cols-1 sm:grid-cols-3 mx-2 gap-y-2 sm:gap-1">
+            <div class="border-2 p-2 flex justify-center sm:col-span-2">
+                <img src="{{ asset('img/')}}" alt="">
+            </div>
+            <div class="border-2 p-2 flex justify-center">A</div>
+            <div class="border-2 p-2 flex justify-center">A</div>
+        </div>
     </main>
     @include('footer')
 </body>
