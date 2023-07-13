7772357c2eb37d380eac879600624d4378c147d7
DanielEspitia1507 <134316312+DanielEspitia1507@users.noreply.github.com>
2023-07-13 06:54:19 -0500

    Revert "Footer"
    This reverts commit b943127668e2c8c2906b7399e4c41d49d02b4de7.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,16 @@
     @include('navbar')
     <main class="container my-10 text-justify mx-auto flex-grow">
     </main>
-    @include('footer')
+    <footer class="font-thin border-double border-t-4 border-black mx-12 p-3">
+        <div class="flex justify-center gap-x-3 p-3 text-lg drop-shadow-xl icons">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-tiktok"></i>
+        </div>
+        <div class="px-3 mb-7 sm:mb-3 text-center text-sm drop-shadow-md texto">
+            <p>&copy; Derechos reservados - PcGlobal - 2023</p>
+        </div>
+    </footer>
 </body>
 </html>
