b943127668e2c8c2906b7399e4c41d49d02b4de7
DanielEspitia1507 <134316312+DanielEspitia1507@users.noreply.github.com>
2023-07-13 06:44:05 -0500

    Footer

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,6 +1,6 @@
-<header class="h-24 bg-indigo-600">
+<header class="h-24">
     <nav class="mx-auto sm:mx-12 2xl:mx-64 p-2 list-none flex gap-5 items-center h-full font-medium bg-transparent justify-around sm:justify-between text-md 2xl:text-xl">
         <li><a class="hover:text-gray-700" href="{{route('index')}}"><i class="fa-solid fa-house"></i></a></li>
-        <li><a class="hover:text-gray-700" href="#"><i class="fa fa-regular fa-heart"></i></a></li>    
+        <li><a class="hover:text-gray-700" href="{{route('proof')}}"><i class="fa fa-regular fa-heart"></i></a></li>    
     </nav>
 </header>
--- resources/views/proof.blade.php
+++ resources/views/proof.blade.php
@@ -6,9 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{asset('fontawesome-free-6.4.0-web/css/all.min.css')}}">
-    <title>Restaurant</title>
+    <title>PcGlobal</title>
 </head>
 <body class="flex flex-col min-h-screen">
     @include('navbar')
+    <main class="container my-10 text-justify mx-auto flex-grow">
+    </main>
+    @include('footer')
 </body>
 </html>
