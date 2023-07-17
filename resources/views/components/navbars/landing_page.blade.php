7c28ad4a1b1a2a6034989df4f259a583c99f1b21
DanielEspitia1507 <134316312+DanielEspitia1507@users.noreply.github.com>
2023-07-17 08:40:59 -0500

    Último cambio del navbar (LO JURO)

--- resources/views/components/navbars/landing_page.blade.php
+++ resources/views/components/navbars/landing_page.blade.php
@@ -1,35 +1,45 @@
 <!-- Barra de Navegación -->
 <header class="h-24 bg-indigo-600">
-    <nav class="relative mx-16 list-none grid grid-cols-3 font-medium bg-transparent justify-around sm:justify-center text-md text-white h-full items-center text-sm gap-x-6 gap-y-3">
+  <nav class="mx-4 sm:mx-12 flex font-medium text-white h-full items-center text-sm gap-x-6 gap-y-3">
+    <div class="nav-1 flex flex-col sm:flex-row items-center justify-between w-full h-full">
+      <!-- Logo #1 (Para pantallas más grandes que las de celular) -->
+      <div class="nav-logo-pag pe-10 py-2 hidden sm:flex">
+        <a href="{{route('index')}}" class="text-white hover:text-gray-400">
+          <p>Logo</p>
+        </a>
+      </div>
       <!-- Formulario de búsqueda -->
-      <div class="col-span-3 sm:w-full md:w-2/3 lg:w-1/2 mx-auto justify-center">
+      <div class="flex flex-grow px-2 sm:px-4 justify-center">
         <form action="#" class="flex nav-search">
-          <input type="text" class="w-full rounded-l-lg px-3 py-2 focus:ring-2 focus:ring-sky-500 text-gray-800" placeholder="¿Qué estás buscando?">
-          <button class="bg-white py-2 px-4 rounded-r-lg focus:outline-none focus:ring-2 focus:ring-sky-500">
+          <input type="text" class="w-full sm:w-96 rounded-l-lg px-3 py-2 focus:ring-2 focus:ring-sky-500 text-gray-800" placeholder="¿Qué estás buscando?">
+          <button class="w-16 bg-white py-2 px-4 rounded-r-lg focus:outline-none focus:ring-2 focus:ring-sky-500">
             <i class="fa-sharp fa-solid fa-magnifying-glass text-gray-700 text-lg"></i>
           </button>
         </form>
-        </div>
-        <!-- Logo -->
-      <div class="nav-logo-pag justify-center mx-auto sm:pe-20 col-span-3 sm:col-auto flex gap-4">
-        <a href="{{route('index')}}" class="text-white hover:text-gray-400">
-            <p>Logo</p>
-        </a>
-        {{-- Ícono del menú al lado del logo --}}
-        <a href="#" class="text-white hover:text-gray-400 flex items-center sm:hidden">
-            <i class="fa-solid fa-bars"></i>
-        </a>
-      </div>        
-      <!-- Enlaces de productos -->
-      <div class="nav-productos justify-evenly hidden sm:flex">
+      </div>
+      <div class="nav-right flex gap-12">
+        <!-- Enlaces de productos -->
+        <div class="nav-productos justify-evenly hidden sm:flex gap-4 ps-3">
           <p><a class="hover:text-gray-200" href="{{url('../#categorias')}}">Categorías</a></p>
           <p><a class="hover:text-gray-200" href="#">PQRS</a></p>
-      </div>      
-      <!-- Enlaces de login y carrito -->
-      <div class="nav-login justify-evenly hidden sm:flex">
-        <a href="{{route("login")}}" class="hover:text-gray-200 -me-12"><i class="fa-regular fa-user"></i></a>
-        <div class="border"></div>
-        <p><a href="" class="hover:text-gray-200 -ms-12"><i class="fa-solid fa-cart-shopping"></i></a></p>
+        </div>
+        <!-- Enlaces de login y carrito -->
+        <div class="nav-login hidden sm:flex gap-4">
+          <a href="{{route("login")}}" class="hover:text-gray-200">
+            <i class="fa-regular fa-user"></i>
+          </a>
+          <div class="border"></div>
+          <p><a href="" class="hover:text-gray-200"><i class="fa-solid fa-cart-shopping"></i></a></p>
+        </div>
+        <!-- Logo #2 (Este es para el responsive) -->
+        <div class="nav-logo-pag py-2 flex sm:hidden gap-3 justify-between">
+          <a href="{{route('index')}}" class="text-white hover:text-gray-400">
+            <p>Logo</p>
+          </a>
+          {{-- Ícono del menú --}}
+          <p><i class="fa-solid fa-bars"></i></p>
+        </div>
       </div>
-    </nav>
-  </header>  
+    </div>
+  </nav>
+</header>
